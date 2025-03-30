e3cbc6b1fd621c23d21583dc98b9543ba2396088
disilva021 <disilva.profissional@gmail.com>
2025-03-30 21:40:32 +0100

    final

--- doxydoc/html/search/all_5.js
+++ doxydoc/html/search/all_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['lerlistaficheiro_0',['LerListaFicheiro',['../funcoes_8c.html#af8d3c506f21e8f02909dc64f438b9eff',1,'LerListaFicheiro(Casa *mapa, EfeitoNefasto *cabeca):&#160;funcoes.c'],['../funcoes_8h.html#af8d3c506f21e8f02909dc64f438b9eff',1,'LerListaFicheiro(Casa *mapa, EfeitoNefasto *cabeca):&#160;funcoes.c']]],
+  ['lerlistaficheiro_0',['LerListaFicheiro',['../funcoes_8c.html#ab6a962a9190367f7f9f902b37a097195',1,'LerListaFicheiro(Casa *mapa, EfeitoNefasto **cabeca):&#160;funcoes.c'],['../funcoes_8h.html#ab6a962a9190367f7f9f902b37a097195',1,'LerListaFicheiro(Casa *mapa, EfeitoNefasto **cabeca):&#160;funcoes.c']]],
   ['limpamemoria_1',['LimpaMemoria',['../funcoes_8c.html#ac3047c35234c5d91be31629239e8a94d',1,'LimpaMemoria(Casa *mapa):&#160;funcoes.c'],['../funcoes_8h.html#ac3047c35234c5d91be31629239e8a94d',1,'LimpaMemoria(Casa *mapa):&#160;funcoes.c']]],
   ['limpamemoriaefeito_2',['LimpaMemoriaEfeito',['../funcoes_8c.html#aafb91a30a66792a37dfa04968a207468',1,'LimpaMemoriaEfeito(EfeitoNefasto *efeito):&#160;funcoes.c'],['../funcoes_8h.html#aafb91a30a66792a37dfa04968a207468',1,'LimpaMemoriaEfeito(EfeitoNefasto *efeito):&#160;funcoes.c']]]
 ];
